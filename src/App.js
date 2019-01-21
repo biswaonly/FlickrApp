c5147000e89c525ee351979d154ab775379eda36
shubh648 <shubhamg648@gmail.com>
2019-01-21 16:37:04 +0530

    ...

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
-import Groups from './component/groups/Groups';
 import './App.css';
 import Header from './component/header/Header';
 import { Route , withRouter } from "react-router-dom";
+import Groups from './component/groups/Groups';
 import GroupEntry from './component/GroupEntry/GroupEntry';
 
 
@@ -11,7 +11,7 @@ class App extends Component {
     return (
       <div className="container-fluid">
         <Header />
-        <Route exact path="/" Component={Groups}></Route>
+        <Route exact path="/" component={Groups}></Route>
         <Route path="/group-entry" component={GroupEntry}></Route> 
       </div>
     );
